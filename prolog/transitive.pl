21820d5b4e3177ddb4a1d09739e8c2f99db4185f
michaelgathara <mikegtrm@gmail.com>
2023-09-11 00:48:23 -0500

    fixed transitive

--- prolog/transitive.pl
+++ prolog/transitive.pl
@@ -6,9 +6,10 @@
         Then A > C
 */
 
-rel(1, 2)
-rel(2, 3)
-rel(3, 4)
+rel(1, 2).
+rel(2, 3).
+rel(3, 4).
 
-transitive(X, Y) :- rel(X, Y), rel(Y, Z)
+transitive(X, Y) :- rel(X, Y).
+transitive(X, Y) :- rel(X, Z), rel(Z, Y).
 /* Transitive should hold all transitive closures */
